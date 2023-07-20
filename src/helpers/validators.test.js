975d6aadb3ca2a825cb64c027cf93b5121796be3
Semyon Rozhkov <semro@outlook.com>
2023-07-20 22:29:52 +0300

    test: add stress tests

--- src/helpers/validators.test.js
+++ src/helpers/validators.test.js
@@ -1,7 +1,14 @@
 import { COLORS, SHAPES } from "../constants";
-import { validateFieldN1, validateFieldN2 } from "./validators";
+import * as validatorsFunctional from "./validators";
+import * as validatorsImperative from "./validatorsImperative";
 
-const shapes = [SHAPES.CIRCLE, SHAPES.SQUARE, SHAPES.TRIANGLE, SHAPES.STAR];
+import "lodash.multipermutations";
+import _ from "lodash";
+
+const shapes = Object.values(SHAPES);
+const colors = Object.values(COLORS);
+
+const colorsPermutations = _.multipermutations(colors, shapes.length);
 
 const paintShapes = (colors) => {
   return shapes.reduce((accum, shape, i) => {
@@ -10,74 +17,29 @@ const paintShapes = (colors) => {
   }, {});
 };
 
-describe("N1 Красная звезда, зеленый квадрат, все остальные белые", () => {
-  test("Если все цвета ок, то true", () => {
-    const colors = [COLORS.WHITE, COLORS.GREEN, COLORS.WHITE, COLORS.RED];
-    const shapes = paintShapes(colors);
+const cases = [
+  "N1 Красная звезда, зеленый квадрат, все остальные белые",
+  "N2 Как минимум две фигуры зеленые",
+  "N3 Количество красных фигур равно кол-ву синих",
+  "N4 Синий круг, красная звезда, оранжевый квадрат, треугольник любого цвета",
+  "N5 Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true)",
+  "N6 Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия",
+  "N7 Все фигуры оранжевые",
+  "N8 Не красная и не белая звезда, остальные – любого цвета",
+  "N9 Все фигуры зеленые",
+  "N10 Треугольник и квадрат одного цвета (не белого)",
+];
 
-    expect(validateFieldN1(shapes)).toBe(true);
+for (let i = 0; i < cases.length; i++) {
+  const testName = cases[i];
+  const functionName = `validateFieldN${i + 1}`;
+
+  test(testName, () => {
+    for (let colors of colorsPermutations) {
+      const shapes = paintShapes(colors);
+      expect(validatorsImperative[functionName](shapes)).toBe(
+        validatorsFunctional[functionName](shapes)
+      );
+    }
   });
-  test("звезда должна быть красная", () => {
-    const colors = [COLORS.WHITE, COLORS.GREEN, COLORS.WHITE, COLORS.WHITE];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN1(shapes)).toBe(false);
-  });
-  test("квадрат должен быть зелёный", () => {
-    const colors = [COLORS.WHITE, COLORS.BLUE, COLORS.WHITE, COLORS.RED];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN1(shapes)).toBe(false);
-  });
-  test("круг должен быть белый", () => {
-    const colors = [COLORS.RED, COLORS.GREEN, COLORS.WHITE, COLORS.RED];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN1(shapes)).toBe(false);
-  });
-  test("треугольник должен быть белый", () => {
-    const colors = [COLORS.WHITE, COLORS.GREEN, COLORS.GREEN, COLORS.RED];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN1(shapes)).toBe(false);
-  });
-});
-
-describe("N2  Как минимум две фигуры зеленые", () => {
-  test("4 зелёных", () => {
-    const colors = [COLORS.GREEN, COLORS.GREEN, COLORS.GREEN, COLORS.GREEN];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN2(shapes)).toBe(true);
-  });
-  test("3 зелёных и белая", () => {
-    const colors = [COLORS.GREEN, COLORS.GREEN, COLORS.WHITE, COLORS.GREEN];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN2(shapes)).toBe(true);
-  });
-  test("3 зелёных и красная", () => {
-    const colors = [COLORS.RED, COLORS.GREEN, COLORS.GREEN, COLORS.GREEN];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN2(shapes)).toBe(true);
-  });
-  test("2 зелёных и 2 красных", () => {
-    const colors = [COLORS.RED, COLORS.RED, COLORS.GREEN, COLORS.GREEN];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN2(shapes)).toBe(true);
-  });
-  test("1 зелёная, 1 синяя и 2 красных", () => {
-    const colors = [COLORS.RED, COLORS.BLUE, COLORS.GREEN, COLORS.RED];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN2(shapes)).toBe(false);
-  });
-  test("0 зелёная, 2 синяя и 2 красных", () => {
-    const colors = [COLORS.BLUE, COLORS.BLUE, COLORS.RED, COLORS.RED];
-    const shapes = paintShapes(colors);
-
-    expect(validateFieldN2(shapes)).toBe(false);
-  });
-});
+}
---
+++ src/helpers/validatorsImperative.js
@@ -0,0 +1,35 @@
+// 1. Красная звезда, зеленый квадрат, все остальные белые.
+export const validateFieldN1 = ({star, square, triangle, circle}) => {
+    if (triangle !== 'white' || circle !== 'white') {
+        return false;
+    }
+
+    return star === 'red' && square === 'green';
+};
+
+// 2. Как минимум две фигуры зеленые.
+export const validateFieldN2 = () => false;
+
+// 3. Количество красных фигур равно кол-ву синих.
+export const validateFieldN3 = () => false;
+
+// 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
+export const validateFieldN4 = () => false;
+
+// 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
+export const validateFieldN5 = () => false;
+
+// 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
+export const validateFieldN6 = () => false;
+
+// 7. Все фигуры оранжевые.
+export const validateFieldN7 = () => false;
+
+// 8. Не красная и не белая звезда, остальные – любого цвета.
+export const validateFieldN8 = () => false;
+
+// 9. Все фигуры зеленые.
+export const validateFieldN9 = () => false;
+
+// 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
+export const validateFieldN10 = () => false;
